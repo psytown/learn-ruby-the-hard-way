addd9a7f99df7c7028638173dac4e8c143e1bb32
psytown <psytown@qq.com>
2012-03-06 16:19:29 +0800

    修改老阳的

--- ex00.rb
+++ ex00.rb
@@ -15,3 +15,14 @@ p "hello,world\n"
 p 'hello,world'
 p 'hello,world\n'
 
+##Authoer,Zhangxi
+
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
